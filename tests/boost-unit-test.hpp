9ee99b9888ada9195f2c2568b85395a856547226
Henrik S. Gaßmann <henrik@gassmann.onl>
2017-09-16 21:25:27 +0200

    Initial project commit

---
+++ tests/boost-unit-test.hpp
@@ -0,0 +1,25 @@
+// Copyright 2015 Henrik Steffen Gaßmann
+//
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+//
+#pragma once
+
+#include <boost/predef.h>
+
+#if defined BOOST_COMP_GNUC_AVAILABLE
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#endif
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/parameterized_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+
+#if defined BOOST_COMP_GNUC_AVAILABLE
+#pragma GCC diagnostic pop
+#endif
---
+++ tests/misc-tests.cpp
@@ -0,0 +1,12 @@
+// Copyright 2017 Henrik Steffen Gaßmann
+//
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+//
+#include <command_parser.hpp>
+#include "boost-unit-test.hpp"
+
+BOOST_AUTO_TEST_SUITE(misc_tests)
+
+
+BOOST_AUTO_TEST_SUITE_END()
